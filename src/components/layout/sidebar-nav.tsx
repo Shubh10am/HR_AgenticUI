bfc6e506e9a35a9e6481ff14279418586c79b21c
Shubham Singh <shubham12342019@gmail.com>
2025-05-30 16:53:36 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <SidebarContent>
    
    with the text content:
    
        DashboardEmail AssistanceGmail InboxAttendanceCommunicationsRecruitmentSmart DraftingTasksIntegrations
    
    Relevant files:
    
        - https://6000-firebase-studio-1747735061156.cluster-6dx7corvpngoivimwvvljgokdw.cloudworkstations.dev/_next/static/chunks/src_27715b33._.js
        - https://6000-firebase-studio-1747735061156.cluster-6dx7corvpngoivimwvvljgokdw.cloudworkstations.dev/_next/static/chunks/src_27715b33._.js
    
    And change it as follows:
    
    on clicking this in mobile or tab view it should toggle

--- src/components/layout/sidebar-nav.tsx
+++ src/components/layout/sidebar-nav.tsx
@@ -20,6 +20,7 @@ import {
   ListChecks, // Added ListChecks icon
 } from 'lucide-react';
 import type { LucideIcon } from 'lucide-react';
+import { useSidebar } from '@/components/ui/sidebar'; // Import useSidebar
 
 interface NavItem {
   href: string;
@@ -42,20 +43,29 @@ const navItems: NavItem[] = [
 
 export default function SidebarNav() {
   const pathname = usePathname();
+  const { isMobile, setOpenMobile } = useSidebar(); // Get sidebar context
+
+  const handleLinkClick = () => {
+    if (isMobile) {
+      setOpenMobile(false); // Close sidebar on mobile after click
+    }
+  };
 
   return (
     <SidebarMenu>
       {navItems.map((item) => (
         <SidebarMenuItem key={item.href}>
           <Link href={item.href} passHref legacyBehavior>
-            <SidebarMenuButton
-              isActive={pathname === item.href || (item.href !== "/" && pathname.startsWith(item.href))}
-              tooltip={item.tooltip}
-              className="justify-start group-data-[collapsible=icon]:justify-center"
-            >
-              <item.icon className="h-5 w-5" />
-              <span className="group-data-[collapsible=icon]:hidden">{item.label}</span>
-            </SidebarMenuButton>
+            <a onClick={handleLinkClick} className="block w-full"> {/* Added <a> tag and onClick */}
+              <SidebarMenuButton
+                isActive={pathname === item.href || (item.href !== "/" && pathname.startsWith(item.href))}
+                tooltip={item.tooltip}
+                className="justify-start group-data-[collapsible=icon]:justify-center"
+              >
+                <item.icon className="h-5 w-5" />
+                <span className="group-data-[collapsible=icon]:hidden">{item.label}</span>
+              </SidebarMenuButton>
+            </a>
           </Link>
         </SidebarMenuItem>
       ))}
