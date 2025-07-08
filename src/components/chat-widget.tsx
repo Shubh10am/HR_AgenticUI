8feac07be9fa01071450e283a4a7fcead745201f
Shubham Singh <shubham12342019@gmail.com>
2025-07-08 14:54:07 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <ChatWidget>
    
    with

--- src/components/chat-widget.tsx
+++ src/components/chat-widget.tsx
@@ -17,10 +17,10 @@ export default function ChatWidget() {
       {/* Chat Widget Window */}
       <div
         className={cn(
-          "transition-all duration-300 ease-in-out",
+          "transition-[opacity,transform] duration-300 ease-in-out origin-bottom-right",
           isOpen
-            ? "visible opacity-100 translate-y-0"
-            : "invisible opacity-0 translate-y-4 pointer-events-none"
+            ? "visible opacity-100 translate-y-0 scale-100"
+            : "invisible opacity-0 translate-y-4 scale-95 pointer-events-none"
         )}
       >
         <div className="w-[350px] h-[calc(100vh-100px)] max-h-[700px] bg-neutral-50 dark:bg-neutral-900 rounded-2xl shadow-2xl flex flex-col overflow-hidden mb-2 border border-border">
